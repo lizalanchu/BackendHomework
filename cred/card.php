82f24b728d65f79e62c692b4aeddcda0860cc76d
lizalanchu <81166835+lizalanchu@users.noreply.github.com>
2023-02-20 16:45:47 +0300

    2

---
+++ cred/card.php
@@ -0,0 +1,25 @@
+<?php include_once ('ver/index.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Card</title>
+</head>
+<body>
+<form action= '' method="post">
+    <input type="number" name="card">
+    <input type="submit" name="send" value="Отправить">
+</form>
+<?php
+if (!empty(($_POST))){
+$Card = $_POST['card'];
+$value = new Card;
+$massage = $value->emit_card($Card);
+echo $massage;}
+?>  
+
+
+
+
+</body>
+</html>
